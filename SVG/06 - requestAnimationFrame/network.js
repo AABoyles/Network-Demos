6d24a66c1f72df1f50713771d0178c8340cbf156
Tony Boyles <tony@ephemer.ai>
2020-12-08 00:29:40 -0500

    Make 06 work

--- SVG/06 - requestAnimationFrame/network.js
+++ SVG/06 - requestAnimationFrame/network.js
@@ -1,70 +1,80 @@
-let svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+'use strict';
 
-let color = d3.scaleOrdinal(d3.schemeCategory10);
+let svg = d3.select("svg")
+	.attr("viewBox", [0, 0, width, height]);
 
-let nnodes = 10000;
-let nlinks = 10000;
-let randomNode = d3.randomInt(nnodes);
-let nodes = [...Array(nnodes).keys()].map(i => ({id: i}));
-let links = [...Array(nlinks).keys()].map(i => ({id: i, source: i % nnodes, target: randomNode()}));
+let g = svg.append("g");
 
-let link = svg.append("g")
-  .attr("class", "links")
+let link = g.append("g")
+    .attr("class", "links")
+    .attr("stroke", "#999")
+    .attr("stroke-opacity", 0.6)
   .selectAll("line")
   .data(links)
-  .enter().append("line")
-  .attr("stroke-width", d => Math.sqrt(d.value));
+  .join("line")
+    .attr("stroke-width", d => Math.sqrt(d.value));
 
-let node = svg.append("g")
+let node = g.append("g")
   .attr("class", "nodes")
-  .selectAll("g")
+  .attr("stroke", "#fff")
+  .attr("stroke-width", 1.5)
+  .selectAll("circle")
   .data(nodes)
-  .enter().append("g")
+  .join("circle")
+    .attr("r", 5)
+    .attr("fill", "#0000ff")
+    .call(d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended));
 
-let circles = node.append("circle")
-  .attr("r", 5)
-  .attr("fill", d => color(d.group))
-  .call(d3.drag()
-    .on("start", dragstarted)
-    .on("drag", dragged)
-    .on("end", dragended));
-
-node.append("title")
-  .text(d => d.id);
-
-function dragstarted(d) {
-  d.fx = d.x;
-  d.fy = d.y;
+function dragstarted(event) {
+  if (!event.active) simulation.alphaTarget(0.3).restart();
+  event.subject.fx = event.subject.x;
+  event.subject.fy = event.subject.y;
 }
 
-function dragged(d) {
-  d.fx = d3.event.x;
-  d.fy = d3.event.y;
+function dragged(event) {
+  event.subject.fx = event.x;
+  event.subject.fy = event.y;
 }
 
-function dragended(d) {
-  d.fx = null;
-  d.fy = null;
+function dragended(event) {
+  if (!event.active) simulation.alphaTarget(0);
+  event.subject.fx = null;
+  event.subject.fy = null;
 }
 
+svg.call(
+  d3.zoom().scaleExtent([1 / 10, 8])
+    .on("zoom", e => {
+      g.attr("transform", e.transform)
+      moveItems();
+    }));
+
+let simulation = d3.forceSimulation()
+  .force("center", d3.forceCenter(width / 2, height / 2))
+  .force("x", d3.forceX(width / 2).strength(0.1))
+  .force("y", d3.forceY(height / 2).strength(0.1))
+  .force("charge", d3.forceManyBody().strength(-100))
+  .force("link", d3.forceLink().strength(0.2).id(d => d.id))
+  .alphaTarget(0)
+  .alphaDecay(0.05);
 
 let moveItems = (function(){
   let todoNode = 0;
   let todoLink = 0;
-  let MAX_NODES = nnodes;
-  let MAX_LINKS = MAX_NODES/2;
+  let MAX_NODES = n/5;
+  let MAX_LINKS = MAX_NODES;
 
   let restart = false;
 
   function moveSomeNodes(){
-    let goal = Math.min(todoNode+MAX_NODES, nnodes);
+    let goal = Math.min(todoNode+MAX_NODES, n);
 
     for(let i = todoNode; i < goal; i++){
       let n = node._groups[0][i];
-      n.setAttribute('x', n.__data__.x);
-      n.setAttribute('y', n.__data__.y);
+      n.setAttribute('transform', `translate(${n.__data__.x}, ${n.__data__.y})`);
     }
 
     todoNode = goal;
@@ -73,9 +83,9 @@ let moveItems = (function(){
 
   function moveSomeLinks(){
     let l;
-    let goal = Math.min(todoLink+MAX_LINKS, nlinks);
+    let goal = Math.min(todoLink+MAX_LINKS, n);
 
-    for(let i=todoLink ; i < goal ; i++){
+    for(let i=todoLink; i < goal; i++){
       l = link._groups[0][i];
       l.setAttribute('x1', l.__data__.source.x);
       l.setAttribute('y1', l.__data__.source.y);
@@ -88,11 +98,11 @@ let moveItems = (function(){
   }
 
   function moveSome(){
-    if(todoNode < nnodes) // some more nodes to do
-    moveSomeNodes()
-    else if(todoLink < nlinks) // some more links to do
-    moveSomeLinks()
-    else if(restart){
+    if(todoNode < n){
+      moveSomeNodes();
+    } else if(todoLink < n){
+      moveSomeLinks();
+    } else if(restart){
       restart = false;
       todoNode = 0;
       todoLink = 0;
@@ -101,17 +111,13 @@ let moveItems = (function(){
   }
 
   return function moveItems(){
+    ticks++;
     if(!restart){
       restart = true;
       requestAnimationFrame(moveSome);
     }
   };
-
 })();
 
-force = d3.forceSimulation(nodes)
-  .force('link', d3.forceLink(links).id(d => d.id))
-  .force('charge', d3.forceManyBody().strength(-250))
-  .force('center', d3.forceCenter());
-
-force.on('tick', moveItems);
+simulation.nodes(nodes).on("tick", moveItems);
+simulation.force("link").links(links);
